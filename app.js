10bdf0bf8894ddf688fd0408fb62d5f3176ce174
lucatolton <41690024+lucatolton@users.noreply.github.com>
2022-02-15 11:42:26 +0000

    feat: add if logic

--- app.js
+++ app.js
@@ -12,9 +12,22 @@ console.log('\n----- Starting -----');
 run.forEach((item) => {
     console.log('');
     console.log(item);
-    let toExec = item.replace(/ *\([^)]*\) */g, "");
-    let execData = item.replace( /(^.*\(|\).*$)/g, '' );
+    let toExec;
+    let execData;
+    if (typeof item === 'object') {
+        for (let key in item) {
+            toExec = key.replace(/ *\([^)]*\) */g, "");
+            execData = item[key];
+        }
+    } else {
+        toExec = item.replace(/ *\([^)]*\) */g, "");
+        execData = item.replace( /(^.*\(|\).*$)/g, '' );
+    }
     
+    runCode(item, toExec, execData);
+});
+
+function runCode(item, toExec, execData) {
     switch (toExec) {
         case 'print':
             console.log('[LOG] ' + execData);
@@ -35,11 +48,45 @@ run.forEach((item) => {
             let getValue = variables.find(item => item.name === getData[0]);
             console.log('[GET] Variable \'' + getData[0] + '\' is \'' + getValue.value + '\'');
             break;
+        case 'if':
+            for (let key in item) {
+                let todo = key.slice(3).slice(0, -1);
+                todo = todo.split(' ');
+                let itemOne = variables.find(item => item.name === todo[0]);
+                let itemTwo = variables.find(item => item.name === todo[2]);
+                if (!itemOne || !itemTwo) {
+                    console.log('[ERROR] Variable \'' + todo[0] + '\' or \'' + todo[2] + '\' does not exist');
+                    break;
+                }
+                if (itemOne.value === itemTwo.value) {
+                    toExec = item[key].replace(/ *\([^)]*\) */g, "");
+                    execData = item[key].replace( /(^.*\(|\).*$)/g, '' );
+                    runCode(item[key], toExec, execData);
+                }
+            }
+            break;
+        case 'ifnot':
+            for (let key in item) {
+                let todo = key.slice(6).slice(0, -1);
+                todo = todo.split(' ');
+                let itemOne = variables.find(item => item.name === todo[0]);
+                let itemTwo = variables.find(item => item.name === todo[2]);
+                if (!itemOne || !itemTwo) {
+                    console.log('[ERROR] Variable \'' + todo[0] + '\' or \'' + todo[2] + '\' does not exist');
+                    break;
+                }
+                if (itemOne.value !== itemTwo.value) {
+                    toExec = item[key].replace(/ *\([^)]*\) */g, "");
+                    execData = item[key].replace( /(^.*\(|\).*$)/g, '' );
+                    runCode(item[key], toExec, execData);
+                }
+            }
+            break;
         case 'exit':
             console.log('[EXIT] Exiting...');
             process.exit(1);
     }
-});
+}
 
 console.log('\n----- Finished -----');
 setTimeout(() => {
